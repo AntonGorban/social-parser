a9532001b1760eb53c0710bc69e2022e5ce3bb7c
AntonGorban <antogor.work@gmail.com>
2021-06-13 19:21:58 +0300

    Parsing screen UI

--- app/src/Screens/Parsing/Parsing.jsx
+++ app/src/Screens/Parsing/Parsing.jsx
@@ -1,7 +1,134 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import api from "../../api/api";
 import { ParsingPresentation } from "./ParsingPresentation";
 
 export const Parsing = () => {
-  return <ParsingPresentation />;
+  const [resources, setResources] = useState({
+    savedResources: [
+      {
+        info: {
+          id: 0,
+          name: "",
+          url: "",
+          vk: "",
+          tg: "",
+          youTube: "",
+          ok: "",
+          inst: "",
+          tw: "",
+        },
+        data: {
+          dayViews: null,
+          dayVisitors: null,
+          weekViews: null,
+          weekVisitors: null,
+          monthViews: null,
+          monthVisitors: null,
+          vk: null,
+          tg: null,
+          youTubeSubscribers: null,
+          youTubeViews: null,
+          ok: null,
+          inst: null,
+          tw: null,
+        },
+      },
+    ],
+    recd: false,
+  });
+
+  const getResources = async () => {
+    const response = await api.resource.get();
+    setResources((prev) => ({
+      ...prev,
+      recd: true,
+      savedResources: response.data.map((resource) => ({
+        info: {
+          id: resource.id,
+          name: resource.name || "",
+          url: resource.url || "",
+          vk: resource.vk || "",
+          tg: resource.tg || "",
+          youTube: resource.youtube || "",
+          ok: resource.ok || "",
+          inst: resource.inst || "",
+          tw: resource.tw || "",
+        },
+        data: {
+          dayViews: null,
+          dayVisitors: null,
+          weekViews: null,
+          weekVisitors: null,
+          monthViews: null,
+          monthVisitors: null,
+          vk: null,
+          tg: null,
+          youTubeSubscribers: null,
+          youTubeViews: null,
+          ok: null,
+          inst: null,
+          tw: null,
+        },
+      })),
+    }));
+  };
+
+  if (!resources.recd) getResources();
+
+  const [tokens, setTokens] = useState({
+    user: {
+      vk: "",
+      tg: "",
+      youTube: "",
+      instLogin: "",
+      instPassword: "",
+      recd: false,
+    },
+    default: {
+      vk: "",
+      tg: "",
+      youTube: "",
+      instLogin: "",
+      instPassword: "",
+      recd: false,
+    },
+  });
+
+  const getUserTokens = async () => {
+    const response = await api.token.get();
+    setTokens((prev) => ({
+      ...prev,
+      user: {
+        vk: response.data.vk,
+        tg: response.data.tg,
+        youTube: response.data.youtube,
+        instLogin: response.data.instLogin,
+        instPassword: response.data.instPassword,
+        recd: true,
+      },
+    }));
+  };
+
+  if (!tokens.user.recd) getUserTokens();
+
+  const getDefaultTokens = async () => {
+    const response = await api.token.default();
+    setTokens((prev) => ({
+      ...prev,
+      default: {
+        vk: response.data.vk,
+        tg: response.data.tg,
+        youTube: response.data.youtube,
+        instLogin: response.data.instLogin,
+        instPassword: response.data.instPassword,
+        recd: true,
+      },
+    }));
+  };
+
+  if (!tokens.default.recd) getDefaultTokens();
+
+  console.log(resources, tokens);
+
+  return <ParsingPresentation resources={resources} />;
 };
--- app/src/Screens/Parsing/ParsingPresentation.jsx
+++ app/src/Screens/Parsing/ParsingPresentation.jsx
@@ -5,10 +5,117 @@ import { Error } from "../../Components/Error/Error";
 import { Input } from "../../Components/Input/Input";
 import { Button } from "../../Components/Button/Button";
 
-export const ParsingPresentation = () => {
+const Td = ({ data, info }) => (
+  <td className={`${classes.col} ${!info && classes.col_none}`}>
+    {info && (data || "\u2015")}
+  </td>
+);
+
+export const ParsingPresentation = ({ resources }) => {
   return (
     <div className={`container ${classes.wrap}`}>
-      <h1>Parsing</h1>
+      <h1>Парсинг</h1>
+      <div className={classes.buttons}>
+        <Button text="Спарсить" icon="fas fa-bolt" onClick={null} />
+        <Button text="Сохранить" icon="fas fa-save" onClick={null} />
+      </div>
+      <table className={classes.table}>
+        <thead className={classes.header}>
+          <tr className={classes.row}>
+            <td className={classes.col} rowspan={3}>
+              ресурс
+            </td>
+            <td className={classes.col} colspan={6} title="метрика">
+              <i className={`${classes.icon} fa fa-chart-line`} />
+            </td>
+            <td className={classes.col} rowspan={3} title="ВКонтакте">
+              <i className={`${classes.icon} fab fa-vk`} />
+            </td>
+            <td className={classes.col} rowspan={3} title="Telegram">
+              <i className={`${classes.icon} fab fa-telegram-plane`} />
+            </td>
+            <td className={classes.col} colspan={2} title="YouTube">
+              <i className={`${classes.icon} fab fa-youtube`} />
+            </td>
+            <td className={classes.col} rowspan={3} title="Одноклассники">
+              <i className={`${classes.icon} fab fa-odnoklassniki`} />
+            </td>
+            <td className={classes.col} rowspan={3} title="Instagram">
+              <i className={`${classes.icon} fab fa-instagram`} />
+            </td>
+            <td className={classes.col} rowspan={3} title="Twitter">
+              <i className={`${classes.icon} fab fa-twitter`} />
+            </td>
+          </tr>
+          <tr className={classes.row}>
+            <td className={classes.col} colspan={2} title="в день">
+              <i className={`${classes.icon} fas fa-calendar-day`} />
+            </td>
+            <td className={classes.col} colspan={2} title="в неделю">
+              <i className={`${classes.icon} fas fa-calendar-week`} />
+            </td>
+            <td className={classes.col} colspan={2} title="в месяц">
+              <i className={`${classes.icon} far fa-calendar`} />
+            </td>
+            <td className={classes.col} rowspan={2} title="подписчиков">
+              <i className={`${classes.icon} fa fa-users`} />
+            </td>
+            <td className={classes.col} rowspan={2} title="просмотров">
+              <i className={`${classes.icon} fa fa-eye`} />
+            </td>
+          </tr>
+          <tr className={classes.row}>
+            <td className={classes.col} title="просмотров">
+              <i className={`${classes.icon} fa fa-eye`} />
+            </td>
+            <td className={classes.col} title="посетителей">
+              <i className={`${classes.icon} fa fa-users`} />
+            </td>
+            <td className={classes.col} title="просмотров">
+              <i className={`${classes.icon} fa fa-eye`} />
+            </td>
+            <td className={classes.col} title="посетителей">
+              <i className={`${classes.icon} fa fa-users`} />
+            </td>
+            <td className={classes.col} title="просмотров">
+              <i className={`${classes.icon} fa fa-eye`} />
+            </td>
+            <td className={classes.col} title="посетителей">
+              <i className={`${classes.icon} fa fa-users`} />
+            </td>
+          </tr>
+        </thead>
+        <tbody className={classes.body}>
+          {resources.savedResources.map((resource) => (
+            <tr className={classes.row}>
+              <td className={`${classes.col} ${classes.col_name}`}>
+                <a href={resource.info.url} target="_blank" rel="noreferrer">
+                  {resource.info.name}
+                </a>
+              </td>
+              <Td data={resource.data.dayViews} info={resource.info.url} />
+              <Td data={resource.data.dayVisitors} info={resource.info.url} />
+              <Td data={resource.data.weekViews} info={resource.info.url} />
+              <Td data={resource.data.weekVisitors} info={resource.info.url} />
+              <Td data={resource.data.monthViews} info={resource.info.url} />
+              <Td data={resource.data.monthVisitors} info={resource.info.url} />
+              <Td data={resource.data.vk} info={resource.info.vk} />
+              <Td data={resource.data.tg} info={resource.info.tg} />
+              <Td
+                data={resource.data.youTubeSubscribers}
+                info={resource.info.youTube}
+              />
+              <Td
+                data={resource.data.youTubeViews}
+                info={resource.info.youTube}
+              />
+              <Td data={resource.data.ok} info={resource.info.ok} />
+              <Td data={resource.data.inst} info={resource.info.inst} />
+              <Td data={resource.data.tw} info={resource.info.tw} />
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
