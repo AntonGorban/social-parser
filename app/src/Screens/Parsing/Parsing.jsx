b203d9d76956c5e50e9c7e3bf3a5d6907f91670c
AntonGorban <antogor.work@gmail.com>
2021-06-13 15:23:41 +0300

    init Parsing screen

---
+++ app/src/Screens/Parsing/Parsing.jsx
@@ -0,0 +1,7 @@
+import React, { useState, eseEffect } from "react";
+import api from "../../api/api";
+import { ParsingPresentation } from "./ParsingPresentation";
+
+export const Parsing = () => {
+  return <ParsingPresentation />;
+};
---
+++ app/src/Screens/Parsing/ParsingPresentation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Parsing.Module.css";
+
+import { Error } from "../../Components/Error/Error";
+import { Input } from "../../Components/Input/Input";
+import { Button } from "../../Components/Button/Button";
+
+export const ParsingPresentation = () => {
+  return (
+    <div className={`container ${classes.wrap}`}>
+      <h1>Parsing</h1>
+    </div>
+  );
+};
