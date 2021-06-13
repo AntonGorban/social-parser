1fa6f29064b010684e293c4716fad31339db35f2
AntonGorban <antogor.work@gmail.com>
2021-06-13 15:26:01 +0300

    hotfix useEffect sintax

--- app/src/Screens/Stats/Stats.jsx
+++ app/src/Screens/Stats/Stats.jsx
@@ -1,4 +1,4 @@
-import React, { useState, eseEffect } from "react";
+import React, { useState, useEffect } from "react";
 import api from "../../api/api";
 import { StatsPresentation } from "./StatsPresentation";
 
