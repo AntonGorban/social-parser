3f178013b53b6d51c01b758de7e657855938d4cd
AntonGorban <antogor.work@gmail.com>
2021-06-13 23:28:13 +0300

    Stats screen UI

--- app/src/Screens/Stats/Stats.jsx
+++ app/src/Screens/Stats/Stats.jsx
@@ -1,7 +1,95 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import api from "../../api/api";
 import { StatsPresentation } from "./StatsPresentation";
 
 export const Stats = () => {
-  return <StatsPresentation />;
+  const [data, setData] = useState({
+    resources: [
+      {
+        info: {
+          id: 5,
+          name: "Первый Республиканский",
+          url: "https://republic-tv.ru",
+        },
+        data: [
+          {
+            date: "2021-05-05T11:30:07.353Z",
+            dayViews: 490,
+            dayVisitors: 130,
+            weekViews: 3430,
+            weekVisitors: 910,
+            monthViews: 14600,
+            monthVisitors: 588,
+            vk: 47800,
+            tg: 24707609,
+            youTubeSubscribers: null,
+            youTubeViews: null,
+            ok: null,
+            inst: null,
+            tw: null,
+          },
+          {
+            date: "2021-06-10T11:30:07.353Z",
+            dayViews: 480,
+            dayVisitors: 127,
+            weekViews: 3440,
+            weekVisitors: 896,
+            monthViews: 14530,
+            monthVisitors: 531,
+            vk: 47760,
+            tg: 2470710,
+            youTubeSubscribers: null,
+            youTubeViews: null,
+            ok: null,
+            inst: null,
+            tw: null,
+          },
+        ],
+      },
+      {
+        info: {
+          id: 6,
+          name: "Народный совет",
+          url: "https://dnrsovet.su",
+        },
+        data: [
+          {
+            date: "2021-05-10T11:30:07.353Z",
+            dayViews: 3600,
+            dayVisitors: 1980,
+            weekViews: 25200,
+            weekVisitors: 13300,
+            monthViews: 112000,
+            monthVisitors: 61300,
+            vk: 12464,
+            tg: 1787,
+            youTubeSubscribers: 0,
+            youTubeViews: 144551,
+            ok: 1986,
+            inst: 1144,
+            tw: 3117,
+          },
+          {
+            date: "2021-06-10T11:30:07.353Z",
+            dayViews: 3580,
+            dayVisitors: 1960,
+            weekViews: 25170,
+            weekVisitors: 13290,
+            monthViews: 111870,
+            monthVisitors: 61300,
+            vk: 12457,
+            tg: 1756,
+            youTubeSubscribers: 0,
+            youTubeViews: 144532,
+            ok: 1973,
+            inst: 1119,
+            tw: 3103,
+          },
+        ],
+      },
+    ],
+    recd: false,
+  });
+
+  return <StatsPresentation resources={data.resources} />;
 };
--- app/src/Screens/Stats/StatsPresentation.jsx
+++ app/src/Screens/Stats/StatsPresentation.jsx
@@ -5,10 +5,132 @@ import { Error } from "../../Components/Error/Error";
 import { Input } from "../../Components/Input/Input";
 import { Button } from "../../Components/Button/Button";
 
-export const StatsPresentation = () => {
+const Td = ({ data }) => (
+  <td className={`${classes.col} ${data === null && classes.col_none}`}>
+    {data !== null
+      ? String(data)
+          .split("")
+          .reverse()
+          .join("")
+          .match(/.{1,3}/g)
+          .join(" ")
+          .split("")
+          .reverse()
+          .join("")
+      : ""}
+  </td>
+);
+
+export const StatsPresentation = ({ resources }) => {
   return (
     <div className={`container ${classes.wrap}`}>
-      <h1>Stats</h1>
+      <h1>Статистика</h1>
+      {resources.map((resource) => (
+        <>
+          <h2>
+            <a href={resource.info.url} target="_blank" rel="noreferrer">
+              {resource.info.name}
+            </a>
+          </h2>
+          <table className={classes.table}>
+            <thead className={classes.header}>
+              <tr className={classes.row}>
+                <td className={classes.col} rowspan={3}>
+                  Дата
+                </td>
+                <td className={classes.col} colspan={6} title="метрика">
+                  <i className={`${classes.icon} fa fa-chart-line`} />
+                </td>
+                <td className={classes.col} rowspan={3} title="ВКонтакте">
+                  <i className={`${classes.icon} fab fa-vk`} />
+                </td>
+                <td className={classes.col} rowspan={3} title="Telegram">
+                  <i className={`${classes.icon} fab fa-telegram-plane`} />
+                </td>
+                <td className={classes.col} colspan={2} title="YouTube">
+                  <i className={`${classes.icon} fab fa-youtube`} />
+                </td>
+                <td className={classes.col} rowspan={3} title="Одноклассники">
+                  <i className={`${classes.icon} fab fa-odnoklassniki`} />
+                </td>
+                <td className={classes.col} rowspan={3} title="Instagram">
+                  <i className={`${classes.icon} fab fa-instagram`} />
+                </td>
+                <td className={classes.col} rowspan={3} title="Twitter">
+                  <i className={`${classes.icon} fab fa-twitter`} />
+                </td>
+              </tr>
+              <tr className={classes.row}>
+                <td className={classes.col} colspan={2} title="в день">
+                  <i className={`${classes.icon} fas fa-calendar-day`} />
+                </td>
+                <td className={classes.col} colspan={2} title="в неделю">
+                  <i className={`${classes.icon} fas fa-calendar-week`} />
+                </td>
+                <td className={classes.col} colspan={2} title="в месяц">
+                  <i className={`${classes.icon} far fa-calendar`} />
+                </td>
+                <td className={classes.col} rowspan={2} title="подписчиков">
+                  <i className={`${classes.icon} fa fa-users`} />
+                </td>
+                <td className={classes.col} rowspan={2} title="просмотров">
+                  <i className={`${classes.icon} fa fa-eye`} />
+                </td>
+              </tr>
+              <tr className={classes.row}>
+                <td className={classes.col} title="просмотров">
+                  <i className={`${classes.icon} fa fa-eye`} />
+                </td>
+                <td className={classes.col} title="посетителей">
+                  <i className={`${classes.icon} fa fa-users`} />
+                </td>
+                <td className={classes.col} title="просмотров">
+                  <i className={`${classes.icon} fa fa-eye`} />
+                </td>
+                <td className={classes.col} title="посетителей">
+                  <i className={`${classes.icon} fa fa-users`} />
+                </td>
+                <td className={classes.col} title="просмотров">
+                  <i className={`${classes.icon} fa fa-eye`} />
+                </td>
+                <td className={classes.col} title="посетителей">
+                  <i className={`${classes.icon} fa fa-users`} />
+                </td>
+              </tr>
+            </thead>
+            <tbody className={classes.body}>
+              {resource.data.map((data) => (
+                <tr className={classes.row}>
+                  <td className={`${classes.col} ${classes.col_name}`}>
+                    {`${
+                      new Date(data.date).getDate() >= 10
+                        ? new Date(data.date).getDate()
+                        : "0" + new Date(data.date).getDate()
+                    }.${
+                      new Date(data.date).getMonth() >= 10
+                        ? new Date(data.date).getMonth()
+                        : "0" + new Date(data.date).getMonth()
+                    }.${new Date(data.date).getFullYear()}`}
+                  </td>
+                  <Td data={data.dayViews} />
+                  <Td data={data.dayVisitors} />
+                  <Td data={data.weekViews} />
+                  <Td data={data.weekVisitors} />
+                  <Td data={data.monthViews} />
+                  <Td data={data.monthVisitors} />
+                  <Td data={data.vk} />
+                  <Td data={data.tg} />
+                  <Td data={data.youTubeSubscribers} />
+                  <Td data={data.youTubeViews} />
+                  <Td data={data.ok} />
+                  <Td data={data.inst} />
+                  <Td data={data.tw} />
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      ))}
     </div>
   );
 };
