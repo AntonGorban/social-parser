dd43861279407a1056ac9c75cd39924079932753
AntonGorban <antogor.work@gmail.com>
2021-06-13 15:24:38 +0300

    create routing

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,23 @@
 import classes from "./App.Module.css";
+
 import { Switch, Route, Redirect } from "react-router-dom";
-import { AuthContainer } from "./Screens/Auth/AuthContainer";
-import { ResourcesContainer } from "./Screens/Resources/ResourcesContainer";
 import { Nav } from "./Components/Nav/Nav";
+
+import { AuthContainer } from "./Screens/Auth/AuthContainer";
+import { Resources } from "./Screens/Resources/Resources";
+import { Settings } from "./Screens/Settings/Settings";
+import { Parsing } from "./Screens/Parsing/Parsing";
+import { Stats } from "./Screens/Stats/Stats";
 
 function App({ isAuth }) {
   return (
     <div>
       {isAuth ? <Nav /> : ""}
       <Switch>
-        <Route exact path="/resources" component={ResourcesContainer} />
+        <Route exact path="/settings" component={Settings} />
+        <Route exact path="/resources" component={Resources} />
+        <Route exact path="/parsing" component={Parsing} />
+        <Route exact path="/stats" component={Stats} />
         <Route exact path="/auth" component={AuthContainer} />
         <Redirect to="/auth" />
       </Switch>
