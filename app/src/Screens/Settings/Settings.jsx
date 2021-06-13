d0e1573e6393ef9c2ad6a63ebeba506eb5e0cb47
AntonGorban <antogor.work@gmail.com>
2021-06-13 15:51:49 +0300

    create state in Settings screen

--- app/src/Screens/Settings/Settings.jsx
+++ app/src/Screens/Settings/Settings.jsx
@@ -3,5 +3,33 @@ import api from "../../api/api";
 import { SettingsPresentation } from "./SettingsPresentation";
 
 export const Settings = () => {
+  const [tokens, setTokens] = useState({
+    vk: "",
+    tg: "",
+    youtube: "",
+    instLogin: "",
+    instPassword: "",
+    recd: false,
+    error: "",
+  });
+
+  const getTokens = async () => {
+    const response = await api.token.get();
+    setTokens({
+      vk: response.data.vk,
+      tg: response.data.tg,
+      youtube: response.data.youtube,
+      instLogin: response.data.instLogin,
+      instPassword: response.data.instPassword,
+      recd: true,
+      error: "",
+    });
+  };
+
+  if (!tokens.recd) {
+    getTokens();
+  }
+
+  console.log("tokens", tokens);
   return <SettingsPresentation />;
 };
