5bbc6d15d2b7a8ef38fe633701848d1caa64c172
AntonGorban <antogor.work@gmail.com>
2021-06-13 15:24:18 +0300

    init Stats screen

---
+++ app/src/Screens/Stats/Stats.jsx
@@ -0,0 +1,7 @@
+import React, { useState, eseEffect } from "react";
+import api from "../../api/api";
+import { StatsPresentation } from "./StatsPresentation";
+
+export const Stats = () => {
+  return <StatsPresentation />;
+};
---
+++ app/src/Screens/Stats/StatsPresentation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Stats.Module.css";
+
+import { Error } from "../../Components/Error/Error";
+import { Input } from "../../Components/Input/Input";
+import { Button } from "../../Components/Button/Button";
+
+export const StatsPresentation = () => {
+  return (
+    <div className={`container ${classes.wrap}`}>
+      <h1>Stats</h1>
+    </div>
+  );
+};
